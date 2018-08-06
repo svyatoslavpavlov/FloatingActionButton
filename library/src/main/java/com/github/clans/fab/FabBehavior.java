ac2a5a79bac1ededd0b4ce7983b86065b8d898a1
svyatoslavdp <svyatoslavdp@yandex-team.ru>
2018-08-06 16:35:01 +0300

    size ratio

---
+++ library/src/main/java/com/github/clans/fab/FabBehavior.java
@@ -0,0 +1,25 @@
+package com.github.clans.fab;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FabBehavior extends CoordinatorLayout.Behavior<FloatingActionMenu> {
+
+    public FabBehavior(Context context, AttributeSet attrs) {
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionMenu child, View dependency) {
+        return dependency instanceof Snackbar.SnackbarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionMenu child, View dependency) {
+        float translationY = Math.min(0, dependency.getTranslationY() - dependency.getHeight());
+        child.setTranslationY(translationY);
+        return true;
+    }
+}
